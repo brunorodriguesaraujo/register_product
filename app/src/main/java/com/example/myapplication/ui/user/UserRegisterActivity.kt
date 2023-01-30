2ad7c74157bc8331449ad91337ef64daf4b88d44
Bruno Rodrigues <brunorodrigues-78@hotmail.com>
2023-01-29 21:09:19 -0300

    feat: #add error input of register

--- app/src/main/java/com/example/myapplication/ui/user/UserRegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/user/UserRegisterActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.ui.user
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.lifecycleScope
 import com.example.myapplication.R
@@ -22,10 +23,9 @@ class UserRegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityUserRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        isEnabled()
+        buttonIsEnabled()
         setTitle()
         setFields()
-        setErrorFields()
         setListener()
     }
 
@@ -33,29 +33,38 @@ class UserRegisterActivity : AppCompatActivity() {
         title = getString(R.string.register_user)
     }
 
-    private fun isEnabled() {
+    private fun buttonIsEnabled() {
         binding.btnSave.isEnabled = userModel.isValid()
     }
 
     private fun setFields() = with(binding) {
-        textInputEditTextUsername.doOnTextChanged { _, _, _, _ ->
-            userModel = userModel.copy(username = textInputEditTextUsername.text.toString())
-            isEnabled()
+        textInputEditTextUsername.apply {
+            doOnTextChanged { _, _, _, _ ->
+                userModel = userModel.copy(username = this.text.toString())
+                buttonIsEnabled()
+            }
+            doAfterTextChanged {
+                textInputLayoutUsername.setErrorEmptyField(this.text.toString())
+            }
         }
-        textInputEditTextName.doOnTextChanged { _, _, _, _ ->
-            userModel = userModel.copy(name = textInputEditTextName.text.toString())
-            isEnabled()
+        textInputEditTextName.apply {
+            doOnTextChanged { _, _, _, _ ->
+                userModel = userModel.copy(name = this.text.toString())
+                buttonIsEnabled()
+            }
+            doAfterTextChanged {
+                textInputLayoutName.setErrorEmptyField(this.text.toString())
+            }
         }
-        textInputEditTextPassword.doOnTextChanged { _, _, _, _ ->
-            userModel = userModel.copy(password = textInputEditTextPassword.text.toString())
-            isEnabled()
+        textInputEditTextPassword.apply {
+            doOnTextChanged { _, _, _, _ ->
+                userModel = userModel.copy(password = this.text.toString())
+                buttonIsEnabled()
+            }
+            doAfterTextChanged {
+                textInputLayoutPassword.setErrorEmptyField(this.text.toString())
+            }
         }
-    }
-
-    private fun setErrorFields() = with(binding) {
-        textInputLayoutUsername.setErrorEmptyField(textInputEditTextUsername.text.toString())
-        textInputLayoutName.setErrorEmptyField(textInputEditTextName.text.toString())
-        textInputLayoutPassword.setErrorEmptyField(textInputEditTextPassword.text.toString())
     }
 
     private fun setListener() = with(binding) {
