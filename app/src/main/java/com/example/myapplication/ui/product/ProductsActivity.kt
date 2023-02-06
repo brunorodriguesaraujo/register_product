9a32ce93ebfc11da300a7e1f6991ab6696d01df4
Bruno Rodrigues <brunorodrigues-78@hotmail.com>
2023-02-05 22:07:24 -0300

    feat: get user by id

--- app/src/main/java/com/example/myapplication/ui/product/ProductsActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/product/ProductsActivity.kt
@@ -3,16 +3,20 @@ package com.example.myapplication.ui.product
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.lifecycle.lifecycleScope
 import com.example.myapplication.constants.ID
+import com.example.myapplication.constants.USER_ID
 import com.example.myapplication.database.AppDatabase
 import com.example.myapplication.databinding.ActivityProductsBinding
+import com.example.myapplication.preferences.dataStore
 import kotlinx.coroutines.launch
 
 class ProductsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProductsBinding
     private val productDAO by lazy { AppDatabase.instance(this).productDao() }
+    private val userDAO by lazy { AppDatabase.instance(this).userDao() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +24,22 @@ class ProductsActivity : AppCompatActivity() {
         setContentView(binding.root)
         setListener()
         setAdapter()
+        getUserById()
     }
 
     private fun setListener() {
         binding.productsFab.setOnClickListener {
             startActivity(Intent(this, ProductRegisterActivity::class.java))
+        }
+    }
+
+    private fun getUserById() {
+        lifecycleScope.launch {
+            dataStore.data.collect { preferences ->
+                preferences[longPreferencesKey(USER_ID)]?.let { id ->
+                    userDAO.getUserById(id)
+                }
+            }
         }
     }
 
